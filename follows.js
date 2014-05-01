ca1f8d35bf16714a96f0924604f15610bd8f3877
DaniusKalv <danius.kalvaitis@gmail.com>
2014-05-01 12:39:15 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'p5Y3WqQRPiTr1C6th8LMYp2Y1axjkPYlMjTWYskPCY1afh5x', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [402041242], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Kukuliukas', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 1, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10, // Default interval for data to be displayed (in seconds)
+		dataColor		= 'FFBF00', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
